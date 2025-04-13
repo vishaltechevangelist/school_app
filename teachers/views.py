28845b7cf5542423e5f5b163606a780401edde60
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-13 14:25:38 +0530

    committing teacher form have only name field

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from teachers.forms import TeachersForm
 
 # Create your views here.
+def home(request):
+    form = TeachersForm()
+    return render(request, 'teachers/home.html', {'form':form})
