2ed72a866329766bae0b05dff7117a32825bbcf4
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-14 23:34:52 +0530

    CRUD update, clean methods, custom validator, fields and exclude attribute

--- students/forms.py
+++ students/forms.py
@@ -1,7 +1,57 @@
 from django import forms
 from students.models import Students
+from django.core import validators
+
+def start_s(value):
+    if value[0] != 'S' and value[0] != 's':
+        raise forms.ValidationError("Name should start with 'S'")
 
 class StudentsForm(forms.ModelForm):
+    name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}),
+                           validators=[start_s, validators.MaxLengthValidator(10)],
+                           label='Your Name')
+
     class Meta:
         model = Students
-        fields = ['name', 'email', 'phone_number', 'student_class', 'age', 'bio']
+        # fields = ['name', 'email', 'phone_number', 'student_class', 'age', 'bio']
+        fields = '__all__'  # 2nd approach if all model field need to be displayed on form
+        # exclude = ['email'] # to exclude or not to display fields on form
+        labels = {
+            'name': 'Your Name',
+            'email': 'Your Email',
+            'student_class': 'Student Class',
+            'age': 'Age',
+            'bio': 'Your Details',
+            'phone_number': 'Contact Number'
+            }
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            'student_class':forms.NumberInput(attrs={'class':'form-control'}),
+            'age':forms.NumberInput(attrs={'class':'form-control'}),
+            'bio':forms.Textarea(attrs={'class':'form-control'}),
+            'phone_number':forms.NumberInput(attrs={'class':'form-control'}),
+        }
+        help_texts = {
+            'email':'We only accept gmails'
+        }
+        error_messages = {
+            'name' : {
+                'required': 'Name field is required'
+            },
+            'email' : {
+                'required': 'Email field is required'
+            },  
+        }
+    
+    # def clean(self):
+    #     cleaned_data = super().clean()
+
+    #     name = self.cleaned_data['name']
+    #     email = self.cleaned_data['email']
+
+    #     if name[0] != 'S' and name[0] != 's':
+    #         raise forms.ValidationError("First letter of name should be 's'")
+        
+    #     if email[0] != 'S' and email[0] != 's':
+    #         raise forms.ValidationError("First letter of email should be 's'")
--- students/views.py
+++ students/views.py
@@ -24,18 +24,12 @@ def data(request):
 def update(request, id):
     student = Students.objects.get(id=id)
     if request.method == 'POST':
-        form = StudentsForm(request.POST)
+        form = StudentsForm(request.POST, instance=student)
         if form.is_valid():
-            student.name = form.cleaned_data['name']
-            student.email = form.cleaned_data['email']
-            student.phone_number = form.cleaned_data['phone_number']
-            student.bio = form.cleaned_data['bio']
-            student.id = id
             student.save()
-            return HttpResponseRedirect('/students/thank-you/')
+            return HttpResponseRedirect('/student/all-data/')
     else:
-        form = StudentsForm(initial={'name':student.name, 'email':student.email, 'phone_number':student.phone_number,
-                                 'bio':student.bio, 'age':student.age, 'student_class':student.student_class})
+        form = StudentsForm(instance=student)
     return render(request, 'students/update.html', {'form':form})
 
 def delete(request, id):
