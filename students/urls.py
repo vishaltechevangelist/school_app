c7c077c6fee1a602190990ef27f7e9b0c15fb6c5
vishaltechevangelist <37928313+vishaltechevangelist@users.noreply.github.com>
2025-04-15 15:43:13 +0530

    implementing cookie CRUD operation

--- students/urls.py
+++ students/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('all-data/', views.data),
     path('update/<int:id>/', views.update, name="update"),
     path('delete/<int:id>/', views.delete, name='delete'),
+    path('setCookie/', views.setCookie),
+    path('getCookie/', views.getCookie),
+    path('deleteCookie/', views.deleteCookie),
+    path('updateCookie/', views.updateCookie),
 ]
